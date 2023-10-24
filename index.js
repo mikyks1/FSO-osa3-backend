cd445b6f0aeff0a1563d0f85126ef741e5e810cf
Mikko Ykspetäjä <53930266+mikyks1@users.noreply.github.com>
2023-10-24 13:32:31 +0300

    3.4: puhelinluettelon backend step4

--- index.js
+++ index.js
@@ -50,6 +50,13 @@ app.get("/info", (req, res) => {
     res.send(reply)
 })
 
+app.delete("/api/persons/:id", (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    res.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
