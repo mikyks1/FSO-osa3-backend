1c9f5dd7c16d0981c64eed1e75691c7bab97a5c0
Mikko Ykspetäjä <53930266+mikyks1@users.noreply.github.com>
2023-10-29 14:25:26 +0200

    3.9: puhelinluettelon backend step9

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const morgan = require("morgan")
 const app = express()
+const cors = require("cors")
 
 morgan.token("content", function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :content"))
 app.use(express.json())
+app.use(cors())
 
 let persons = [
     {
